c8f4d92452081d37b764062b17dc761a9cd321f0
Nicolas <1075039737@qq.com>
2021-06-22 18:38:41 +0800

    调整sidebar视觉效果

--- vue-element-admin/src/layout/components/Sidebar/SidebarItem.vue
+++ vue-element-admin/src/layout/components/Sidebar/SidebarItem.vue
@@ -14,6 +14,7 @@
                 <el-menu-item
                     :index="resolvePath(onlyOneChild.path)"
                     :class="{ 'submenu-title-noDropdown': !isNest }"
+                    class="too-long-title"
                 >
                     <item
                         :icon="
@@ -124,3 +125,9 @@ export default {
     },
 };
 </script>
+<style lang='css' scoped>
+.too-long-title {
+    text-overflow: ellipsis;
+    overflow: hidden;
+}
+</style>
