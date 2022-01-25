d30f8d5a2e06056a2082e6773d84533b4d9255e8
Nicolas <1075039737@qq.com>
2022-01-25 22:06:04 +0800

    fix:学生雷达图普通评论标签未改成中文

--- vue-element-admin/src/views/activity/components/timeline-project/components/statistics/index.vue
+++ vue-element-admin/src/views/activity/components/timeline-project/components/statistics/index.vue
@@ -1,30 +1,30 @@
 <template>
-    <!--TODO：可按节显示发言情况，可显示总体发言情况，可显示发言情况排名-->
-    <div class="container" v-loading="!dataRecevied">
-        <div v-if="dataRecevied">
-            <group-chart
-                v-for="(s, index) of chartData"
-                :key="s.name"
-                :chartData="s.data"
-                :classData="
-                    authorType === 'group' && index === chartData.length - 1
-                        ? chartClassData.group
-                        : chartClassData.personal
-                "
-                :indicator="indicator"
-                :title="s.title"
-                :legend="[s.title, '班级平均互动', '班级最高互动']"
-            />
+  <!--TODO：可按节显示发言情况，可显示总体发言情况，可显示发言情况排名-->
+  <div v-loading="!dataRecevied" class="container">
+    <div v-if="dataRecevied">
+      <group-chart
+        v-for="(s, index) of chartData"
+        :key="s.name"
+        :chart-data="s.data"
+        :class-data="
+          authorType === 'group' && index === chartData.length - 1
+            ? chartClassData.group
+            : chartClassData.personal
+        "
+        :indicator="indicator"
+        :title="s.title"
+        :legend="[s.title, '班级平均互动', '班级最高互动']"
+      />
 
-            <StatisticTable :tableData="tableData" />
-        </div>
+      <StatisticTable :table-data="tableData" />
     </div>
+  </div>
 </template>
 
 <script>
-import groupChart from "./components/group.vue"
-import { getTimelineStatisticData } from "@/api/statistic"
-import StatisticTable from "@/components/StatisticTable"
+import groupChart from './components/group.vue'
+import { getTimelineStatisticData } from '@/api/statistic'
+import StatisticTable from '@/components/StatisticTable'
 
 export default {
     components: { groupChart, StatisticTable },
@@ -35,8 +35,8 @@ export default {
             activityID: this.$route.params.id,
             chartClassData: [],
             tableData: [],
-            indicator: [], //评论数，回复数，entry评论数，普通评论
-            authorType: "personal",
+            indicator: [], // 评论数，回复数，entry评论数，普通评论
+            authorType: 'personal'
         }
     },
     created() {
@@ -44,14 +44,19 @@ export default {
     },
     methods: {
         getData() {
-            let activityID = this.activityID
+            const activityID = this.activityID
             getTimelineStatisticData({ activityID })
                 .then(res => {
-                    let { data } = res
-                    let { chartClassData, chartData, indicator, tableData, authorType } = data
+                    const { data } = res
+                    const { chartClassData, chartData, indicator, tableData, authorType } = data
                     this.chartClassData = chartClassData
                     this.chartData = chartData
+
+                    indicator[indicator.length - 1] = {
+                        name: '普通评论'
+                    }
                     this.indicator = indicator
+
                     this.tableData = tableData
                     this.authorType = authorType
                     this.dataRecevied = true
@@ -59,8 +64,8 @@ export default {
                 .catch(err => {
                     console.log(err)
                 })
-        },
-    },
+        }
+    }
 }
 </script>
 
