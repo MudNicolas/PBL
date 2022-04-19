685b1a644bc84ff765807e1641df9546460da1e3
Nicolas <1075039737@qq.com>
2022-04-19 20:46:21 +0800

    添加部分部署注释

--- server/settings.js
+++ server/settings.js
@@ -1,17 +1,35 @@
 let env = process.argv[2]
 
-const SERVER_IP = env ? "http://127.0.0.1:14758" : "https://pbl.jinxy.cn"
+//服务端地址
+const SERVER_IP = "https://pbl.jinxy.cn"
 
+//开发用服务器地址，用于测试，部署时无须修改
+if (env === "dev") {
+    SERVER_IP = "http://127.0.0.1:14758"
+}
+
+//app.listen监听的端口
 const PORT = 14758
+
+//可访问的服务端地址，与SERVER_IP相同
 const SERVER_ADDRESS = `${SERVER_IP}`
+
+//课程封面图片存储地址
 const COVER_PATH = `${SERVER_ADDRESS}/public/img/course/`
+
+//用户头像图片存储地址
 const AVATAR_PATH = `${SERVER_ADDRESS}/public/img/avatar/`
+
+//用户默认密码
 const DEFAULT_PASSWORD = "123456"
-//16位
+
+//非对称加密密钥，必须是16位长度
 const SECRET_KEY = "NicolasAirlineWH"
-//16位
+
+//非堆成加密偏移值，必须是16位长度
 const IV = "WUHUWUHUWUHUWUHU"
 
+//前端网页部署地址
 const PROD_ORIGIN = "http://localhost:9527"
 
 export {
